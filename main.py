d0172da09ede608c1916e424767d7010dc831a65
Lee Azzarello <lee.azzarello@astraspace.com>
2021-12-13 01:14:13 -0800

    get program to make a virtual output to pipewire!

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 import json
-import midi
-# import jack
+import rtmidi
+from time import sleep
+from rtmidi.midiconstants import NOTE_OFF, NOTE_ON
+
+device_name = "MIDI Control Center"
+midiout = rtmidi.MidiOut()
+midiout.open_virtual_port(device_name)
 
 SCREEN_TYPE = 0x04
 SCREEN_CID = 0x20
@@ -13,36 +18,40 @@ MAP_FILE = "ardour-map-export.json"
 # A Drawbar is inverted, up = 0 down = 127
 # It's an organ thing
 fader_option_map = [
-  {"Type": "Fader", "value" : 12},
-  {"Type": "Drawbar", "value" : 13 },
+    {"Type": "Fader", "value": 12},
+    {"Type": "Drawbar", "value": 13},
 ]
 # not sure what the three levels of relative means
 knob_option_map = [
-  {"Type": "Absolute", "value": 0},
-  {"Type": "Relative 1", "value": 1},
-  {"Type": "Relative 2", "value": 2},
-  {"Type": "Relative 3", "value": 3},
+    {"Type": "Absolute", "value": 0},
+    {"Type": "Relative 1", "value": 1},
+    {"Type": "Relative 2", "value": 2},
+    {"Type": "Relative 3", "value": 3},
 ]
 # I think the manual defines in words what these types mean
 knob_acceleration_map = [
-  {"Type": "None", "value": 0},
-  {"Type": "Medium", "value": 1},
-  {"Type": "Fast", "value": 2},
-  {"Type": "1:1", "value": 3},
+    {"Type": "None", "value": 0},
+    {"Type": "Medium", "value": 1},
+    {"Type": "Fast", "value": 2},
+    {"Type": "1:1", "value": 3},
 ]
 pad_option_map = [
-  {"Type": "Gate", "value" : 1},
-  {"Type": "Toggle", "value" : 0},
+    {"Type": "Gate", "value": 1},
+    {"Type": "Toggle", "value": 0},
 ]
 pad_mode_map = [
-  {"Type": "None", "value": 0},
-  {"Type": "MIDI Note", "value": 7},
-  {"Type": "Switched Control", "value": 8},
-  {"Type": "Patch Change", "value": 9},
-  {"Type": "MMC", "value": 10},
+    {"Type": "None", "value": 0},
+    {"Type": "MIDI Note", "value": 7},
+    {"Type": "Switched Control", "value": 8},
+    {"Type": "Patch Change", "value": 9},
+    {"Type": "MMC", "value": 10},
 ]
 pad_mmc_map = [
-  {"Type": "Start", "value": 2},
-  {"Type": "Stop", "value": 3},
-  {"Type": "Record", "value": 4},
+    {"Type": "Start", "value": 2},
+    {"Type": "Stop", "value": 3},
+    {"Type": "Record", "value": 4},
 ]
+
+if __name__ == "__main__":
+    while True:
+        sleep(1)
